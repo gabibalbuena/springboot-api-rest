1a9380e3d843c72991bf622f297293f5ac40f47e
Gabi Balbuena <gabibalbuena123@gmail.com>
2024-10-18 09:27:16 -0300

    Primera actualizacion de proyecto api rest

---
+++ src/main/resources/import.sql
@@ -0,0 +1,21 @@
+INSERT INTO fabricante(id, nombre) VALUES(1, 'Asus');
+INSERT INTO fabricante(id, nombre) VALUES(2, 'HP');
+INSERT INTO fabricante(id, nombre) VALUES(3, 'Dell');
+INSERT INTO fabricante(id, nombre) VALUES(4, 'Lenovo');
+INSERT INTO fabricante(id, nombre) VALUES(5, 'Samsung');
+INSERT INTO fabricante(id, nombre) VALUES(6, 'Seagate');
+INSERT INTO fabricante(id, nombre) VALUES(7, 'Western Digital');
+INSERT INTO fabricante(id, nombre) VALUES(8, 'Sony');
+INSERT INTO fabricante(id, nombre) VALUES(9, 'Apple');
+
+
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(1,'Disco Duro SATA3 1TB', 86.99, 5)
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(2,'Memoria RAM DDR4 8GB', 45.50, 2);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(3,'Monitor 24 pulgadas Full HD', 120.99, 3);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(4,'Teclado mecánico retroiluminado', 70.25, 4);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(5,'SSD 512GB NVMe', 150.00, 6);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(6,'Disco Externo 2TB', 89.99, 7);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(7,'Auriculares Bluetooth', 59.95, 8);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(8,'MacBook Pro 13"', 1299.99, 9);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(9,'Monitor curvo 27 pulgadas', 249.99, 5);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(10,'Disco Duro Externo 4TB', 99.99, 7);
